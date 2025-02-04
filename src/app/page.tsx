97ec56bad1e9264f9b6cfbb08ae2d4aab1e97f79
Ahmed <ahmedtballal@gmail.com>
2025-02-03 16:56:33 -0800

    feat: nav bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import type { ReactElement } from 'react';
 // import { useScrollTracking } from '@/hooks/useScrollTracking'; 1997
 
@@ -10,21 +10,21 @@ import { NavigationBar } from '@/components/navigation/NavigationBar'; // 1997
 import { About } from '@/components/sections/about/About';
 // import { Projects } from '@/components/sections/projects/Projects'; 1997
 import { Contact } from '@/components/sections/contact/Contact';
-import { ProfileCard } from '@/components/profile/ProfileCard';
+// import { ProfileCard } from '@/components/profile/ProfileCard';
 
 export default function Home(): ReactElement {
-  const [isScrolled, setIsScrolled] = useState(false);
+  // const [isScrolled, setIsScrolled] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const aboutSection = document.getElementById('about');
-      const aboutSectionBottom = aboutSection?.getBoundingClientRect().bottom || 0;
-      setIsScrolled(aboutSectionBottom < 0);
-    };
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     const aboutSection = document.getElementById('about');
+  //     const aboutSectionBottom = aboutSection?.getBoundingClientRect().bottom || 0;
+  //     setIsScrolled(aboutSectionBottom < 0);
+  //   };
 
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
+  //   window.addEventListener('scroll', handleScroll);
+  //   return () => window.removeEventListener('scroll', handleScroll);
+  // }, []);
 
   // const [isMounted, setIsMounted] = useState<boolean>(false);
   // // const { isScrolled, activeSection } = useScrollTracking(); 1997
@@ -57,7 +57,7 @@ export default function Home(): ReactElement {
       /> */}
       <main className="pt-16">
         <section id="profile" className="py-12 hidden lg:block">
-          <ProfileCard isScrolled={isScrolled} />
+          {/* <ProfileCard isScrolled={isScrolled} /> */}
         </section>
         <section id="about" className="py-12">
           <About />
