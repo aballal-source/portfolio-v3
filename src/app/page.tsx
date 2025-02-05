ab1cbb5a2dc0226d4c00121cf2bc94ec439a96fc
Ahmed <ahmedtballal@gmail.com>
2025-02-04 18:04:32 -0800

    fix: subtle changes to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -101,7 +101,7 @@ export default function Home(): ReactElement {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center">
-              <Link href="/" className="text-primary text-2xl font-bold">
+              <Link href="/" className="text-primary text-2xl font-bold lg:hidden">
                 Ahmed Ballal
               </Link>
             </div>
